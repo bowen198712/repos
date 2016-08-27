28bcd3883676bd084a9435cb0923af238151555e
Bowen <zhoubowenowen@gmail.com>
2016-08-27 16:58:19 -0400

    added FnOne

---
+++ SomeClass.java
@@ -0,0 +1,11 @@
+public class SomeClass{
+	public void fnOne(){
+	System.out,println("I cound not repair your brakes, so I made your horn louder");
+	}
+
+
+	public static void main(String[] args) {
+	        SomeClass sc = new SomeClass();
+	        sc.fnOne();
+	}
+}
