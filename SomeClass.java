f801bd9012b35da5e01db776ada5ae27f8edc8f9
Bowen <zhoubowenowen@gmail.com>
2016-08-27 17:17:17 -0400

    Added events

--- SomeClass.java
+++ SomeClass.java
@@ -1,11 +1,14 @@
 public class SomeClass{
 	public void thoughtForTheDay(){
-	System.out,println("I cound not repair your brakes, so I made your horn louder");
+	System.out.println("I cound not repair your brakes, so I made your horn louder");
 	}
 
-
+    public void upcomingEvents(){
+    	System.out.println("Party at Jane's house tomorrow");
+    }
 	public static void main(String[] args) {
 	        SomeClass sc = new SomeClass();
 	        sc.thoughtForTheDay();
+	        sc.upcomingEvents();
 	}
 }
